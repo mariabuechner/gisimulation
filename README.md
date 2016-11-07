6b92f644b4c77b8fb661151114d6245c8864ce5c
buechner_m <maria.buechner@psi.ch>
2016-11-07 17:39:35 +0100

    started material luts

--- README.md
+++ README.md
@@ -5,8 +5,6 @@
 
 Simulation toolbox for x-ray grating interferometry
 
-## Installation
+## Installation rewuirements
 
-```bash
-python setup.py develop
-```
+The python package 'nist_lookup', laocated at git@git.psi.ch:tomcat/nist_lookup.git needs to be installed.
--- simulation/interferometer/materials.py
+++ simulation/interferometer/materials.py
@@ -1,18 +1,91 @@
 """
-Module to store and manage materials for gi_simulation
-"""
+Module to retrieve the complexe refractive index: n = 1 - delta - i*beta and
+density rho .
 
+With delta:
+
+    real part of index of refraction, with the resulting attenuation
+    coefficient mu:
+        mu = 4*pi*delta/lambda
+
+With beta:
+
+    complex part of index of refraction
+
+With rho: density in [g/cm3]
+
+
+The python package 'nist_lookup' (git@git.psi.ch:tomcat/nist_lookup.git) is
+used to retrieve delta and beta. This requires the input pf the dentisty
+
+
+"""
 import logging
+import nist_lookup.xraydb_plugin as xdb
+import urllib2
+import numpy as np
+import csv
 logger = logging.getLogger(__name__)
 
-def exists(material_name):
+
+
+
+def delta_beta(material, energy, rho=0):
     """
-    Check if material is in list.
+    .
 
     Parameters
     ==========
 
-    material_name: name as [string]
+
+    Returns
+    =======
+
+
+    Notes
+    =====
+
+
+    Examples
+    ========
+
+    """
+    if rho is not 0:
+        [delta, beta, attenuation_length] = xdb.xray_delta_beta(material, rho,
+        energy)
+        return delta,beta
+    else:
+        density_dict = get_densities()
+        materials = density_dict.keys()
+        rhos = density_dict.values()
+
+def get_densities():
+    """
+    .
+
+    Parameters
+    ==========
+
+
+    Returns
+    =======
+
+
+    Notes
+    =====
+
+
+    Examples
+    ========
+
+    """
+
+def rho(material_name):
+    """
+    .
+
+    Parameters
+    ==========
 
 
     Returns
