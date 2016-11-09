ab6ed731e2767efbb23d8f4c83b4613db7e65fe2
buechner_m <maria.buechner@psi.ch>
2016-11-09 16:42:57 +0100

    grating classes base structure ready

--- simulation/interferometer/gratings.py
+++ simulation/interferometer/gratings.py
@@ -4,7 +4,7 @@ logger = logging.getLogger(__name__)
 
 
 
-class Grating():
+class Grating(object):
     """
     Parent class for gratings.
 
@@ -14,18 +14,24 @@ class Grating():
     pitch: grating pitch in [um], in x-direction
     material: grating material
     design_energy: x-ray energy [keV]
+    height: grating height in [um], in z-direction; default=0 (no height
+    specified)
     duty_cycle: default=0.5
+    shape: shape of grating, choices = ['flat','circular'], default='flat'
 
     Examples
     ========
 
 
     """
-    def __init__(self, pitch, material, design_energy, duty_cycle=0.5):
+    def __init__(self, pitch, material, design_energy, height=0,
+    duty_cycle=0.5, shape='flat'):
         self.pitch = pitch # [um]
         self.material = material
         self.design_energy = design_energy # [keV]
+        self.height = height # [um]
         self.duty_cycle = duty_cycle
+        self.shape = shape
 
 
 class PhaseGrating(Grating):
@@ -35,8 +41,6 @@ class PhaseGrating(Grating):
     Parameters
     ==========
 
-    height: grating height in [um], in z-direction; default=0 (no height
-    specified)
     phase_shift: required phase shift at given design energy; default=0 (no
     shift specified)
 
@@ -51,11 +55,15 @@ class PhaseGrating(Grating):
     ========
 
     """
-    def __init__(self, height=0, phase_shift=0):
-        if height:
-            self.height = height # [um]
-            self.phase_shift = materials.height_to_shift(height, self.material,
-                self.design_energy)
+    def __init__(self, pitch, material, design_energy, height=0,
+    duty_cycle=0.5, shape='flat', phase_shift=0):
+        # call init from parent class
+        super(PhaseGrating, self).__init__(pitch, material, design_energy,
+            height, duty_cycle, shape)
+        # Calculate height or phase shift respectively
+        if self.height:
+            self.phase_shift = materials.height_to_shift(self.height,
+                self.material, self.design_energy)
         elif phase_shift:
             self.height = materials.shift_to_height(phase_shift, self.material,
                 self.design_energy) # [um]
@@ -72,8 +80,6 @@ class AbsorptionGrating(Grating):
     Parameters
     ==========
 
-    height: grating height in [um], in z-direction; default=0 (no height
-    specified)
     absorption: required percentage of absorbed x-rays at design energy;
     default=0 (no absorption specified)
 
@@ -88,11 +94,15 @@ class AbsorptionGrating(Grating):
     ========
 
     """
-    def __init__(self, height=0, absorption=0):
-        if height:
-            self.height = height # [um]
-            self.absorption = materials.height_to_absorption(height, self.material,
-                self.design_energy) # [%]
+    def __init__(self, pitch, material, design_energy, height=0,
+    duty_cycle=0.5, shape='flat', absorption=0):
+        # call init from parent class
+        super(AbsorptionGrating, self).__init__(pitch, material,
+            design_energy, height, duty_cycle, shape)
+        # Calculate height or absorption respectively
+        if self.height:
+            self.absorption = materials.height_to_absorption(self.height,
+                self.material, self.design_energy) # [%]
         elif absorption:
             self.height = materials.absorption_to_height(absorption,
                 self.material, self.design_energy) # [um]
