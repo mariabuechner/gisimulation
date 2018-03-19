79282a12aa5a3ecc1fd6dbf42f340c1947f083cb
Maria Buechner <maria.buechner@gmail.com>
2018-03-19 16:56:59 +0100

    started spec dev

--- gisimulation/simulation/analytical.py
+++ gisimulation/simulation/analytical.py
@@ -18,26 +18,39 @@ logger = logging.getLogger(__name__)
 # Calculations ################################################################
 
 
-#def calculate_results(parameters, results):
-#    """
-#    ...
-#
-#
-#    Returns
-#    =======
-#
-#    analytical_results [dict]
-#
-#    """
-#    parameters = parameters.copy()
-#    geometry_results = results['geometry'].copy()
-#    analytical_results = dict()
-#
-#    # Spectrum development
-#
-#    # Store original input spectrum
-#    analytical_results['energies'] = parameters['spectrum']['energies']
-#    analytical_results['input_photons'] = parameters['spectrum']['photons']
-#    analytical_results['input_photons_relative'] = \
-#        analytical_results['input_photons'] / \
-#        sum(analytical_results['input_photons'])
+def spectrum_development(parameters, analytical_results,
+                         analytical_components):
+    """
+
+    Parameters
+    ==========
+
+    parameters [dict]
+    analytical_results [dict] (as ref)
+    analytical_components [list] (of strings)
+
+    """
+    # Store original input spectrum
+    analytical_results['energies'] = parameters['spectrum']['energies']
+    analytical_results['input_photons'] = parameters['spectrum']['photons']
+    analytical_results['input_photons_relative'] = \
+        analytical_results['input_photons'] / \
+        sum(analytical_results['input_photons'])
+
+    for component in analytical_components:
+
+
+
+def calculate_results(parameters, results, analytical_components):
+    """
+    ...
+
+
+    """
+    parameters = parameters.copy()
+    geometry_results = results['geometry'].copy()
+
+    # Spectrum development
+    spectrum_development(parameters, results['analytical'],
+                         analytical_components)
+
